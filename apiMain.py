e699f3cbd95cd8f9de2bb32e23d43b8b3231dfa4
Marek Sautter <marek.sautter@gmail.com>
2018-09-14 17:18:15 -0700

    Basic Cleanup and Database Creation
    -Illustrated the story of a young programmer named Marek and his battle with Christian programmers.

--- apiMain.py
+++ apiMain.py
@@ -1,75 +1,27 @@
 from flask import Flask, jsonify
+import sqlite3
 
 app = Flask(__name__)
-
-sampleForums = [
-    {
-        'id': 1,
-        'name': "Python",
-        'creator': "Marek"
-    },
-    {
-        'id': 2,
-        'name': "Javascript",
-        'creator': "Jessica"
-    }
-]
-
-sampleThreads = [
-    {
-        "id": 1.1,
-        "title": "What's everyone's favorite thing about Python?",
-        "creator": 'Tony',
-        "timestamp": "Wed, 05 Sep 2018 16:22:29 GMT"
-    },
-    {
-        "id": 1.2,
-        "title": "Any good python libraries I should try?",
-        "creator": "Charlie",
-        "timestamp": "Tue, 04 Sep 2018 13:18:43 GMT"
-    },
-    {
-        "id": 2.1,
-        "title": "Why does everyone like JavaScript?",
-        "creator": 'Tom',
-        "timestamp": "Wed, 05 Sep 2018 13:22:29 GMT"
-    },
-    {
-        "id": 2.2,
-        "title": "JavaScript should be the universal language. Change my mind.",
-        "creator": "Linus",
-        "timestamp": "Tue, 04 Sep 2018 05:21:53 GMT"
-    },
-    {
-        "id": 2.3,
-        "title": "JavaScript tips and tricks.",
-        "creator": "Sally",
-        "timestamp": "Tue, 04 Sep 2018 09:18:43 GMT"
-    }
-]
-
-sampleDiscussion = [
-    {
-        "author": "bob",
-        "text": "I'm trying to connect to MongoDB, but it doesn't seem to be running.",
-        "timestamp": "Tue, 04 Sep 2018 15:42:28 GMT"
-    },
-    {
-        "author": "alice",
-        "text": "Ummm… maybe 'sudo service start mongodb'?",
-        "timestamp": "Tue, 04 Sep 2018 15:45:36 GMT"
-    }
-]
+databaseName = 'databases.db'
+try:
+    conn = sqlite3.connect(databaseName)
+    print('Successfully connect to '+ databaseName)
+except:
+    print("Failed to connect to " + databaseName)
 
 
 @app.route('/forums', methods=['GET'])
 def get_forums():
-    return jsonify({"sampleForums": sampleForums})
+    return "TEST"
+
+@app.route('/forums', methods=['POST'])
+def add_forum():
+    return "TEST"
 
 @app.route('/forums/<int:forum_id>', methods=['GET'])
-def get_Threads(forum_id):
-    
-    return jsonify({"sampleThreads": sampleThreads})
+def get_threads(forum_id):
+    return "TEST"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backup.py
@@ -0,0 +1,26 @@
+
+# @app.route('/forums/<int:forum_id>', methods=['POST'])
+# def add_thread(forum_id):
+#     return 'threadadded', 201
+#     return 'nosuchthread', 404
+
+# @app.route('/forums/<int:forum_id>/<int:thread_id>', methods=['GET'])
+# def get_posts(forum_id, thread_id):
+#     # REPLACE WITH SQL CODE
+#     # Get the forum and then get the threads
+#     # /REPLACE WITH SQL CODE
+#     # return 'threadadded', 201
+#     # return 'nosuchthread', 404
+
+# @app.route('/forums/<int:forum_id>/<int:thread_id>', methods=['POST'])
+# def get_posts(forum_id, thread_id):
+
+# @app.route('/forums/users', methods=['POST'])
+# def add_user():
+
+# @app.route('/forums/users/<string:user_name>', methods=['POST'])
+# def change_password(user_name):
+
+# @app.route('/login'):
+# def login():
+    

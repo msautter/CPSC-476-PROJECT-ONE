bb43b1f90e1488b2bb789c2a2877303cf05e70b5
Marek Sautter <marek.sautter@gmail.com>
2018-09-16 01:50:30 -0700

    Added status codes and location headers
    -Still need finished basic auth

--- apiMain.py
+++ apiMain.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_basicauth import BasicAuth
 import json
 import sqlite3
@@ -6,14 +6,12 @@ import datetime
 
 # sqlite3 auction.db < create.sql
 app = Flask(__name__)
-app.config['BASIC_AUTH_USERNAME'] = 'jesus'
-app.config['BASIC_AUTH_PASSWORD'] = 'password'
+# app.config['BASIC_AUTH_USERNAME'] = 'jesus'
+# app.config['BASIC_AUTH_PASSWORD'] = 'password'
 basic_auth = BasicAuth(app)
 
 
 databaseName = 'databases.db'
-responseJSON = []
-reponse = ""
 
 def connectDB():
     try:
@@ -21,8 +19,9 @@ def connectDB():
         global conn
         conn = sqlite3.connect(databaseName)
         cur = conn.cursor()
+        print("SUCCESS: USER CONNECTED TO DATABASE")
     except:
-        print("Failed to connect to " + databaseName)
+        return "ERROR: DATABASE OFFLINE"
     
 # ********************************************************************
 @app.route('/forums', methods=['GET'])
@@ -30,36 +29,60 @@ def get_forums():
     responseJSON = []
     connectDB()
     cur.execute("SELECT * FROM forums")
-    sqlForums = cur.fetchall()
-    for forum in sqlForums:
+    forumsSQL = cur.fetchall()
+    if forumsSQL == []:
+        return jsonify(forumsSQL)
+    for forum in forumsSQL:
         responseJSON.append({'id': forum[0], 'title': forum[1], 'creator': forum[2]})    
-    return jsonify(responseJSON)
+    responseJSON = jsonify(responseJSON)
+    response = make_response(responseJSON)
+    response.status_code = 200
+    return response
 # ********************************************************************
-
 
 # ********************************************************************
 @app.route('/forums/<int:forum_id>', methods=['GET'])
 def get_threads(forum_id):
-    reponseJSON = []
+    responseJSON = []
     connectDB()
     cur.execute("SELECT * FROM threads WHERE forum_id={}".format(str(forum_id)))
-    sqlThreads = cur.fetchall()
-    for thread in sqlThreads:
-        responseJSON.append({'id': thread[1], 'title': thread[2], 'creator': thread[4], 'timestamp': thread[5]})
-    return jsonify(responseJSON)
+    threadsSQL = cur.fetchall()
+    if threadsSQL == []:
+        response = make_response("ERROR: FORUM NOT FOUND")
+        response.status_code = 404
+        return response
+    for thread in threadsSQL:
+        responseJSON.append({'id': thread[1], 'title': thread[2], 'creator': thread[3], 'timestamp': thread[4]})
+    response = make_response(jsonify(responseJSON))
+    response.status_code = 201
+    return response
 # ********************************************************************
-
 
 # ********************************************************************
 @app.route('/forums/<int:forum_id>/<int:thread_id>', methods=['GET'])
 def get_posts(forum_id, thread_id):
     responseJSON = []
     connectDB()
+    cur.execute("SELECT * FROM forums WHERE forum_id={}".format(str(forum_id)))
+    forumsSQL = cur.fetchall()
+    if forumsSQL == []:
+        response = make_response("ERROR: FORUM NOT FOUND")
+        response.status_code = 404
+        return reponse
+    cur.execute("SELECT * FROM threads WHERE thread_id={}".format(str(thread_id)))
+    threadsSQL = cur.fetchall()
+    if threadsSQL == []:
+        response = make_response("ERROR: THREAD NOT FOUND")
+        response.status_code = 404
+        return reponse
+        
     cur.execute("SELECT * FROM posts WHERE forum_id={} AND thread_id={}".format(str(forum_id), str(thread_id)))
     postsSQL = cur.fetchall()
     for post in postsSQL:
-        responseJSON.append({'author': post[4], 'text': post[3], 'timestamp': post[5]})
-    return jsonify(responseJSON)
+        responseJSON.append({'author': post[3], 'text': post[2], 'timestamp': post[4]})
+    response = make_response(jsonify(responseJSON))
+    response.status_code = 200
+    return response
 # ********************************************************************
 
 
@@ -69,15 +92,75 @@ def add_forum():
     connectDB()
     requestJSON = request.get_json(force=True)
     cur.execute("SELECT * FROM forums WHERE forum_name='"+str(requestJSON['name'])+"'")
-    forums = cur.fetchall()
-    if forums != []:
-        print(requestJSON['name'])
-        cur.execute("INSERT INTO forums(forum_name,forum_creator) VALUES (?,?)",(str(requestJSON['name']), "marek.sautter"))
-        conn.commit()
-        return "MADE IT"
+    forumsSQL = cur.fetchall()
+    if forumsSQL != []:
+            response = make_response("ERROR: FORUM ALREADY EXISTS")
+            response.status_code = 409
+            return response
+    cur.execute("INSERT INTO forums(forum_name,forum_creator) VALUES (?,?,?)",(str(requestJSON['name']), "marek.sautter", str(datetime.datetime.now())))
     conn.commit()
-    return "TEST"
+    cur.execute("SELECT * FROM forums WHERE forum_name={}".format(str(requestJSON['name'])))
+    newForum = cur.fetchone()
+    response = make_response("SUCCESS: FORUM CREATED")
+    response['location'] = '/forums/{}'.format(str(newForum['forum_id']))
+    response.status_code = 201
+    return response
 # ********************************************************************
+
+# ********************************************************************
+@app.route('/forums<int:forum_id>', methods=['POST'])
+def add_thread(forum_id):
+    requestJSON = request.get_json(force=True)
+    currentTime = str(datetime.datetime.now())
+    connectDB()
+    cur.execute("SELECT * FROM forums WHERE forum_id={}".format(str(forum_id)))
+    forumsSQL = cur.fetchall()
+    if forumsSQL == []:
+        response = make_response("ERROR: FORUM NOT FOUND")
+        response.status_code = 404
+        return reponse
+    cur.execute("INSERT INTO threads(forum_id, thread_creator, thread_title, thread_time) VALUES (?,?,?,?)",(forum_id, "marek.sautter", requestJSON['title'], currentTime))
+    conn.commit()
+    cur.execute("INSERT INTO posts(forum_id, post_creator, post_text, post_time) VALUES (?,?,?,?)",(forum_id, "marek.sautter", requestJSON['text'], currentTime))
+    cur.commit()
+
+    cur.execute("SELECT * FROM threads WHERE thread_name={}".format(str(requestJSON['title'])))
+    newThread = cur.fetchone()
+    response = make_response("SUCCESS: THREAD CREATED")
+    response['location'] = '/forums/{}/{}'.format(str(newThread['forum_id']), str(newThread['thread_id']))
+    response.status_code = 201
+    return response
+# ********************************************************************
+
+# ********************************************************************
+@app.route('/forums<int:forum_id>/<int:thread_id>', methods=['POST'])
+def add_post(forum_id, thread_id):
+    requestJSON = request.get_json(force=True)
+    currentTime = str(datetime.datetime.now())
+    connectDB()
+    cur.execute("SELECT * FROM forums WHERE forum_id={}".format(str(forum_id)))
+    forumsSQL = cur.fetchall()
+    if forumsSQL == []:
+        response = make_response("ERROR: FORUM NOT FOUND")
+        response.status_code = 404
+        return response
+    cur.execute("SELECT * FROM threads WHERE thread_id={}".format(str(thread_id)))
+    threadsSQL = cur.fetchall()
+    if threadsSQL == []:
+        response = make_response("ERROR: FORUM NOT FOUND")
+        response.status_code = 404
+        return response
+
+    cur.execute("INSERT INTO posts(forum_id, thread_id, post_creator, post_time) VALUES (?,?,?,?,?)",(forum_id, thread_id, "marek.sautter", requestJSON['text'], currentTime))
+    conn.commit()
+    cur.execute("SELECT * FROM posts WHERE post_name={} AND post_time={}".format(str(requestJSON['text']), str(currentTime)))
+    newThread = cur.fetchone()
+    response = make_response("SUCCESS: POST CREATED")
+    response['location'] = '/forums/{}/{}'.format(str(newThread['forum_id']), str(newThread['thread_id']))
+    response.status_code = 201
+    return response
+# ********************************************************************
+
 
 
 # ********************************************************************
@@ -87,9 +170,17 @@ def add_user():
     username = requestJson['username']
     password = requestJson['password']
     connectDB()
+    cur.execute("SELECT * FROM users WHERE users_name={}",(str(username)))
+    user = cur.fetchall()
+    if user != []:
+        response = make_response("ERROR: USERNAME ALREADY EXISTS")
+        response.status_code = 409
+        return response
     cur.execute("INSERT INTO users(users_name, users_pw) VALUES (?,?)",(str(username), str(password)))
     conn.commit()
-    return  "Hello"
+    response = make_response("SUCCESS: USERNAME CREATED")
+    response.status_code = 201
+    return response
 # ********************************************************************
 
 
@@ -99,16 +190,18 @@ def change_password():
     requestJson = request.get_json(force=True)
     username = requestJson['username']
     password = requestJson['password']
-    print(str(username))
-    print(str(password))
     connectDB()
     cur.execute("SELECT * FROM users WHERE users_name = '{}'".format(username))
-    dbUser = cur.fetchall()
-    if dbUser == []:
-        return "User not found"
+    user = cur.fetchall()
+    if user == []:
+        response = make_response("ERROR: USER NOT FOUND")
+        response.status_code = 404
+        return response
     cur.execute("UPDATE users SET users_pw= '{}' WHERE users_name= '{}'".format(password, username))
     conn.commit()
-    return  "Hello"
+    response = make_response("SUCCESS: PASSWORD CHANGED")
+    response.status_code = 201
+    return response
 # ********************************************************************
 
 if __name__ == '__main__':
--- init.sql
+++ init.sql
@@ -21,7 +21,6 @@ CREATE TABLE threads (
 CREATE TABLE posts (
 	forum_id	INTEGER ,
 	thread_id INTEGER ,
-	post_id INTEGER ,
 	post_text	TEXT,
 	post_creator	TEXT ,
 	post_time TEXT ,
@@ -77,60 +76,60 @@ VALUES (3, "C++", "ray.comfort", "2018-09-14 14:58:28.605170");
 
 INSERT INTO threads (forum_id, thread_id, thread_title, thread_creator, thread_time)
 VALUES (1,1, "Python is the future of programming!","martin.luther", "2018-09-14 15:00:47.128324");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (1,1,1, "I love Python so much I just wanted to make my own thread about it!", "martin.luther", "2018-09-14 15:00:47.128324");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (1,1,2, "Python Smython, It's all about x86 Assembly!", "mary.magdalene", "2018-09-14 15:17:28.082059");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (1,1,3, "Ha, try coding the next facebook on assembly code", "marek.sautter", "2018-09-14 15:31:11.063779");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (1,1, "I love Python so much I just wanted to make my own thread about it!", "martin.luther", "2018-09-14 15:00:47.128324");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (1,1, "Python Smython, It's all about x86 Assembly!", "mary.magdalene", "2018-09-14 15:17:28.082059");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (1,1, "Ha, try coding the next facebook on assembly code", "marek.sautter", "2018-09-14 15:31:11.063779");
 
 
 INSERT INTO threads (forum_id, thread_id, thread_title, thread_creator, thread_time)
 VALUES (1,2, "How do you avoid creating 'spaghetti-code'?", "billy.graham", "2018-09-14 15:03:17.245418");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (1,2,1, "Everytime I start something in Python it just ends up being a huge mess. What are the best practices for OOP principles in Python?", "billy.graham", "2018-09-14 15:03:17.245418");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (1,2,2, "The best way to avoid spaghetti code is to not eat spaghetti while coding", "franklin.graham", "2018-09-14 15:39:13.590829");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (1,2,3, "No, @franklin.graham, you've got the wrong idea. I would say the best way to avoid it is to sketch your code down on a piece of paper before you start", "marek.sautter", "2018-09-14 15:41:00.676886");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (1,2, "Everytime I start something in Python it just ends up being a huge mess. What are the best practices for OOP principles in Python?", "billy.graham", "2018-09-14 15:03:17.245418");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (1,2, "The best way to avoid spaghetti code is to not eat spaghetti while coding", "franklin.graham", "2018-09-14 15:39:13.590829");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (1,2, "No, @franklin.graham, you've got the wrong idea. I would say the best way to avoid it is to sketch your code down on a piece of paper before you start", "marek.sautter", "2018-09-14 15:41:00.676886");
 
 INSERT INTO threads (forum_id, thread_id, thread_title, thread_creator, thread_time)
 VALUES (2,1, "Is Java Dying?", "kirk.cameron", "2018-09-14 15:08:39.121413");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (2,1,1, "My friend made fun of me for coding in Java the other day. He said I was coding in a dead language, is that true?", "kirk.cameron", "2018-09-14 15:08:39.121413");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (2,1,2, "Yup it's dead, switch while you can", "td.jakes", "2018-09-14 15:17:19.593804");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (2,1,3, "Well now hold on @td.jakes, there are still companies that need Java developers. It's not dead yet!", "marek.sautter", "2018-09-14 15:17:19.593804");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (2,1, "My friend made fun of me for coding in Java the other day. He said I was coding in a dead language, is that true?", "kirk.cameron", "2018-09-14 15:08:39.121413");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (2,1, "Yup it's dead, switch while you can", "td.jakes", "2018-09-14 15:17:19.593804");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (2,1, "Well now hold on @td.jakes, there are still companies that need Java developers. It's not dead yet!", "marek.sautter", "2018-09-14 15:17:19.593804");
 
 
 INSERT INTO threads (forum_id, thread_id, thread_title, thread_creator, thread_time)
 VALUES (2,2, "Top 3 Tips For Java", "rick.warren", "2018-09-14 15:10:43.361063");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (2,2,1, "Just started coding in Java, what are your best tips for a newbie?", "rick.warren", "2018-09-14 15:10:43.361063");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (2,2,2, "I use these tips for roasting the best cup of Java: https://food-hacks.wonderhowto.com/how-to/make-perfect-coffee-every-time-with-these-java-pro-tips-0160957/", "nick.vujicic", "2018-09-14 15:17:45.812445");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (2,2,3, "@nick.vujicic, I think he meant Java the programming language. Here's a good video to watch before you start coding in java: https://www.youtube.com/watch?v=dQw4w9WgXcQ", "marek.sautter", "2018-09-14 15:18:19.812445");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (2,2, "Just started coding in Java, what are your best tips for a newbie?", "rick.warren", "2018-09-14 15:10:43.361063");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (2,2, "I use these tips for roasting the best cup of Java: https://food-hacks.wonderhowto.com/how-to/make-perfect-coffee-every-time-with-these-java-pro-tips-0160957/", "nick.vujicic", "2018-09-14 15:17:45.812445");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (2,2, "@nick.vujicic, I think he meant Java the programming language. Here's a good video to watch before you start coding in java: https://www.youtube.com/watch?v=dQw4w9WgXcQ", "marek.sautter", "2018-09-14 15:18:19.812445");
 
 
 INSERT INTO threads (forum_id, thread_id, thread_title, thread_creator, thread_time)
 VALUES (3,1, "Why do schools still teach C++ first", "joseph.smith", "2018-09-14 15:13:38.751101");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (3,1,1, "C++ isn't the most difficult language but it certainly isn't the easiest. Why do most colleges teach such a difficult language to begin with?", "joseph.smith", "2018-09-14 15:13:38.751101");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (3,1,2, "Schools teach C++ first that way the losers that don't know how to code will drop out! More money for those who stay!", "joel.olsteen", "2018-09-14 15:47:05.833942");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (3,1,3, "While I do believe a lot of college freshmen transfer from CPSC because it's difficult, that doesn't mean it should be that way. We need to make it easier for our classmates to do well with coding. We need to make coding fun and available to everyone! And screw you @joel.osteen, you already have enough money, you don't need more!", "marek.sautter", "2018-09-14 15:50:00.048241");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (3,1, "C++ isn't the most difficult language but it certainly isn't the easiest. Why do most colleges teach such a difficult language to begin with?", "joseph.smith", "2018-09-14 15:13:38.751101");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (3,1, "Schools teach C++ first that way the losers that don't know how to code will drop out! More money for those who stay!", "joel.olsteen", "2018-09-14 15:47:05.833942");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (3,1, "While I do believe a lot of college freshmen transfer from CPSC because it's difficult, that doesn't mean it should be that way. We need to make it easier for our classmates to do well with coding. We need to make coding fun and available to everyone! And screw you @joel.osteen, you already have enough money, you don't need more!", "marek.sautter", "2018-09-14 15:50:00.048241");
 
 
 
 INSERT INTO threads (forum_id, thread_id, thread_title, thread_creator, thread_time)
 VALUES (3,2, "Game Development with C++?", "marry.eddy", "2018-09-14 15:15:09.371090");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (3,2,1,"Is C++ still the industry standard for making video games? I'm getting back into the game development business and I have no idea what's relevant these days", "marry.eddy", "2018-09-14 15:15:09.371090");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (3,2,1, "The best games are developed using x86 Assembly.", "mary.magdalene", "2018-09-14 15:51:10.570510");
-INSERT INTO posts (forum_id, thread_id, post_id, post_text, post_creator, post_time)
-VALUES (3,2,2, "God dammnit @mary.magdalene, who the hell let you into this private forum, was it @jesus.christ? C++ is still being used @marry.eddy but I would look into the C#/Unity Combo that's starting to get popular", "marek.sautter", "2018-09-14 15:52:39.208019");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (3,2,"Is C++ still the industry standard for making video games? I'm getting back into the game development business and I have no idea what's relevant these days", "marry.eddy", "2018-09-14 15:15:09.371090");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (3,2,"The best games are developed using x86 Assembly.", "mary.magdalene", "2018-09-14 15:51:10.570510");
+INSERT INTO posts (forum_id, thread_id, post_text, post_creator, post_time)
+VALUES (3,2,"God dammnit @mary.magdalene, who the hell let you into this private forum, was it @jesus.christ? C++ is still being used @marry.eddy but I would look into the C#/Unity Combo that's starting to get popular", "marek.sautter", "2018-09-14 15:52:39.208019");
 
